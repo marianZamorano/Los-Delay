d817215c0596fd692946b5bf3a424f20540f72fe
mxrixn <126999395+mxrixn@users.noreply.github.com>
2023-10-30 15:14:07 -0400

    PantallaPrincipalActividades

---
+++ app/src/main/java/com/example/myfamilyspace/adapter/ActividadesAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.myfamilyspace.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myfamilyspace.R
+import com.example.myfamilyspace.dataClases.DataClaseActividades
+import com.example.myfamilyspace.databinding.ItemActividadActividadesBinding
+import kotlin.random.Random
+
+class ActividadesAdapter : RecyclerView.Adapter<ActividadesAdapter.ActividadesAdapterViewHolder>() {
+
+    private var context: Context? = null
+    private var listaActividades = mutableListOf<DataClaseActividades>()
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ActividadesAdapter.ActividadesAdapterViewHolder {
+        context = parent.context
+        return ActividadesAdapterViewHolder(
+            ItemActividadActividadesBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: ActividadesAdapterViewHolder,
+        position: Int
+    ) {
+        holder.binding(listaActividades[position])
+        holder.binding.fondoActividadActividades.setBackgroundColor(holder.itemView.resources.getColor(getRandomColor(), null))
+    }
+
+    private fun getRandomColor(): Int {
+        val colorCode = mutableListOf<Int>()
+        colorCode.add(R.color.lilaclaro)
+        colorCode.add(R.color.verdeTurquesaClaro)
+        colorCode.add(R.color.aquamarine)
+        colorCode.add(R.color.lightSkyBlue)
+        colorCode.add(R.color.violet)
+        colorCode.add(R.color.verdeAmarillo)
+        val numero = Random.nextInt(colorCode.size)
+        return colorCode.get(numero)
+
+    }
+
+    override fun getItemCount(): Int = listaActividades.size
+
+    inner class ActividadesAdapterViewHolder(val binding: ItemActividadActividadesBinding) :
+        RecyclerView.ViewHolder(binding.root){
+        fun binding(data: DataClaseActividades){
+            binding.tituloActividad.text = data.titulo
+            binding.fecha.text = data.fecha
+            binding.horaInicial.text = data.horaInicial
+            binding.horaFinal.text = data.horaFinal
+        }
+    }
+
+    fun agregarActividades(newDataClaseActividades: List<DataClaseActividades>){
+        listaActividades.clear()
+        listaActividades.addAll(newDataClaseActividades)
+    }
+}
---
+++ app/src/main/java/com/example/myfamilyspace/dataClases/DataClaseActividades.kt
@@ -0,0 +1,8 @@
+package com.example.myfamilyspace.dataClases
+
+data class DataClaseActividades(
+    val titulo: String,
+    val fecha: String,
+    val horaInicial: String,
+    val horaFinal: String
+)
