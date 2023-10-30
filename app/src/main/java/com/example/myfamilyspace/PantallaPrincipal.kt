b15c77d7c4f8ed75502f848e979ca0a4e55547c5
mxrixn <126999395+mxrixn@users.noreply.github.com>
2023-10-30 16:07:41 -0400

    Pantalla Principal a PantallaPrincipalActividades

--- app/src/main/java/com/example/myfamilyspace/PantallaPrincipal.kt
+++ app/src/main/java/com/example/myfamilyspace/PantallaPrincipal.kt
@@ -1,11 +1,25 @@
 package com.example.myfamilyspace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myfamilyspace.databinding.ActivityPantallaPrincipalBinding
 
 class PantallaPrincipal : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPantallaPrincipalBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla_principal)
+        setContentView(binding.root)
+        binding = ActivityPantallaPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botonActividades.setOnClickListener {
+            clickSeccionActividades()
+        }
+    }
+
+    fun clickSeccionActividades() {
+        val intent = Intent(this, PantallaPrincipalActividades::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/myfamilyspace/PantallaPrincipalActividades.kt
+++ app/src/main/java/com/example/myfamilyspace/PantallaPrincipalActividades.kt
@@ -1,11 +1,21 @@
 package com.example.myfamilyspace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myfamilyspace.databinding.ActivityPantallaPrincipalActividadesBinding
 
 class PantallaPrincipalActividades : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPantallaPrincipalActividadesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla_principal_actividades)
+        setContentView(binding.root)
+        binding = ActivityPantallaPrincipalActividadesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.irAPantallaPrincipalBoton.setOnClickListener {
+            val intent = Intent(this, PantallaPrincipal::class.java)
+            startActivity(intent)
+        }
     }
 }
