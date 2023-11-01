bccbc574cbf97e4bc8e16df1f71deaaaf52cbab7
mxrixn <126999395+mxrixn@users.noreply.github.com>
2023-11-01 06:40:36 -0400

    Intento de votaciones juegos

--- app/src/main/java/com/example/myfamilyspace/PantallaVotacionesJuegos.kt
+++ app/src/main/java/com/example/myfamilyspace/PantallaVotacionesJuegos.kt
@@ -1,8 +1,10 @@
 package com.example.myfamilyspace
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myfamilyspace.adapter.VotacionJuegosAdapter
 import com.example.myfamilyspace.dataClases.DataClaseJuegos
@@ -12,6 +14,8 @@ class PantallaVotacionesJuegos : AppCompatActivity() {
 
     private lateinit var binding: ActivityPantallaVotacionesJuegosBinding
     private val VotacionJuegosAdapter by lazy { VotacionJuegosAdapter() }
+    private lateinit var preference: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPantallaVotacionesJuegosBinding.inflate(layoutInflater)
@@ -25,34 +29,31 @@ class PantallaVotacionesJuegos : AppCompatActivity() {
             startActivity(intent)
         }
 
-//        mostrarVotacionesJuegos()
+        mostrarVotacionesJuegos()
     }
 
-//    fun mostrarVotacionesJuegos(){
-//        val primeraJuegoMasVotado = DataClaseJuegos.SubDataClaseVotacionJuegos(
-//            nombreJuego = ,
-//            cantidadVotacionesLikesJuego = ,
-//            cantidadVotacionesDislikesJuego =
-//        )
-//
-//        val segundoJuegoMasVotado = DataClaseJuegos.SubDataClaseVotacionJuegos(
-//            nombreJuego = ,
-//            cantidadVotacionesLikesJuego = ,
-//            cantidadVotacionesDislikesJuego =
-//        )
-//        val tercerJuegoMasVotado = DataClaseJuegos.SubDataClaseVotacionJuegos(
-//            nombreJuego = ,
-//            cantidadVotacionesLikesJuego = ,
-//            cantidadVotacionesDislikesJuego =
-//        )
-//
-//        VotacionJuegosAdapter.agregarVotacionJuegos(listOf(primeraJuegoMasVotado, segundoJuegoMasVotado, tercerJuegoMasVotado))
-//
-//        binding.pantallaVotacionJuegosRecyclerView.apply{
-//            layoutManager =
-//                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-//            adapter = VotacionJuegosAdapter
-//        }
-//    }
+    fun mostrarVotacionesJuegos(){
+        PreferenceManager.getDefaultSharedPreferences(this)
+
+        val likeCount = preference.getInt("LIKE_COUNT", 0)
+        val dislikeCount = preference.getInt("DISLIKE_COUNT", 0)
+
+        val juegosConVotaciones = mutableListOf(
+            DataClaseJuegos.SubDataClaseVotacionJuegos("Nombre del Juego 1", likeCount, dislikeCount),
+            DataClaseJuegos.SubDataClaseVotacionJuegos("Nombre del Juego 2", likeCount, dislikeCount),
+            DataClaseJuegos.SubDataClaseVotacionJuegos("Nombre del Juego 3", likeCount, dislikeCount)
+        )
+
+        juegosConVotaciones.sortByDescending { it.cantidadVotacionesLikesJuego }
+
+        val juegosMostrados = juegosConVotaciones.take(3)
+        VotacionJuegosAdapter.agregarVotacionJuegos(juegosMostrados)
+
+        binding.pantallaVotacionJuegosRecyclerView.apply{
+            layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = VotacionJuegosAdapter
+        }
+    }
 
 }
